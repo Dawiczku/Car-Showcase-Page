0a17eedfa20fa31dc1fc317604abebecdb041287
Dawiczku <skarupski.dawid@gmail.com>
2025-02-26 14:47:10 +0100

    chore: add car data

---
+++ Car Showcase Page/src/components/gallerypage/SideBar.tsx
@@ -0,0 +1,13 @@
+export const SideBar = () => {
+  return (
+    <aside>
+      <div>
+        <svg></svg>
+      </div>
+      <div></div>
+      <div>
+        <a></a>
+      </div>
+    </aside>
+  );
+};
